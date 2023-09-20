e4ebf322051fd7193acb77a8a810cef5c3c66a6f
Lucas Porfírio Marques <114091985+lporfirio@users.noreply.github.com>
2023-09-20 18:00:11 -0300

    primeira entrada

---
+++ data/albuns.js
@@ -0,0 +1,19 @@
+const albuns = {
+  id: "",
+  nome: 'Laugh Track',
+  artista: 'The National',
+  ano: '2023',
+  faixas: [
+    {
+      nome: "Alphabet City",
+      duracao: "3:43"
+    }, {
+      nome: "Deep End", 
+      duracao: "4:29"
+    }, {
+      nome: "Weird Goodbyes",
+      duracao: "5:03"
+    }
+  ]
+} 
+
---
+++ scripts/songsavor.js
@@ -0,0 +1,13 @@
+const sobreTextos = [
+  "O SongSavor é o lugar onde você pode saborear suas músicas favoritas. Crie listas dos seus álbuns prediletos e compartilhe suas descobertas musicais com o mundo.",
+  "Você está na terra encantada do SongSavor, onde a música não é apenas ouvida, é degustada. Crie seus pratos musicais e faça um festim para os seus ouvidos!",
+  "No SongSavor, é como se cada álbum fosse uma fatia de bolo de aniversário - compartilhe, saboreie e celebre suas músicas favoritas conosco!",
+  "Bem-vindo ao SongSavor, onde sua playlist vira um banquete musical! Monte seu cardápio dos álbuns mais saborosos e sirva a melhor música para o mundo."
+];
+
+// Escolhe um texto aleatório
+const textoAleatorio = sobreTextos[Math.floor(Math.random() * sobreTextos.length)];
+
+// Define o texto na div
+const sobreDiv = document.querySelector('.js-sobre-texto');
+sobreDiv.innerHTML = textoAleatorio;
