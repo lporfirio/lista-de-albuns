ae5738362140e07ae2ebcae030f7007d32ce74c0
Lucas Porfírio Marques <114091985+lporfirio@users.noreply.github.com>
2023-09-21 10:25:36 -0300

    atualizando

--- data/albuns.js
+++ data/albuns.js
@@ -1,5 +1,5 @@
-const albuns = {
-  id: "",
+export const albuns = [{
+  id: "tn-12",
   nome: 'Laugh Track',
   artista: 'The National',
   ano: '2023',
@@ -15,5 +15,63 @@ const albuns = {
       duracao: "5:03"
     }
   ]
+}, {
+  id: "fdc03",
+  nome: 'Skinty Fia',
+  artista: 'Fontaines D.C',
+  ano: '2022',
+  faixas: [
+    {
+      nome: "In ár gCroíthe go deo",
+      duracao: "3:43"
+    }, {
+      nome: "Big Shot", 
+      duracao: "4:29"
+    }, {
+      nome: "How Cold Love Is",
+      duracao: "5:03"
+    }
+  ]
+}, {
+  id: "kol08",
+  nome: 'When You See Yourself',
+  artista: 'Kings Of Leon',
+  ano: '2021',
+  faixas: [
+    {
+      nome: "When You See Yoursel, Are You Far Away",
+      duracao: "5:47"
+    }, {
+      nome: "The Bandit", 
+      duracao: "4:10"
+    }, {
+      nome: "100,000 People",
+      duracao: "5:44"
+    }, {
+      nome: "Stormy Weather",
+      duracao: "3:42"
+    }
+  ]
+}, {
+  id: "glz08",
+  nome: 'Time Machine Season One: Strange Timez',
+  artista: 'Gorillaz',
+  ano: '2020',
+  faixas: [
+    {
+      nome: "Strange Timez",
+      duracao: "3:47"
+    }, {
+      nome: "The Valley of The Pagans", 
+      duracao: "3:01"
+    }, {
+      nome: "The Lost Chord",
+      duracao: "4:03"
+    }, {
+      nome: "Pac-Man",
+      duracao: "3:12"
+    }
+  ]
 } 
+]
 
--- scripts/songsavor.js
+++ scripts/songsavor.js
@@ -1,3 +1,5 @@
+import { albuns } from "../data/albuns.js";
+
 const sobreTextos = [
   "O SongSavor é o lugar onde você pode saborear suas músicas favoritas. Crie listas dos seus álbuns prediletos e compartilhe suas descobertas musicais com o mundo.",
   "Você está na terra encantada do SongSavor, onde a música não é apenas ouvida, é degustada. Crie seus pratos musicais e faça um festim para os seus ouvidos!",
@@ -11,3 +13,19 @@ const textoAleatorio = sobreTextos[Math.floor(Math.random() * sobreTextos.length
 // Define o texto na div
 const sobreDiv = document.querySelector('.js-sobre-texto');
 sobreDiv.innerHTML = textoAleatorio;
+
+
+
+// -- Listas de álbuns adicionados
+const albunsAdicionados = document.querySelector('.js-albuns-adicionados');
+
+albunsAdicionados.innerHTML = `
+      <ul>
+        <li>album 1</li>
+        <li>album 2</li>
+        <li>album 3</li>
+        <li>album 4</li>
+      </ul>
+`;
+
+console.log(albuns);
